c8f2f3303b5da7caae9de2e73306b6b3764621b9
cn0379 <cn0379@outlook.com>
2020-09-28 01:07:56 +0800

    code2

---
+++ notes.md
@@ -0,0 +1,5 @@
++ set HTTPS=true&&npm start  开启https协议模式
++ set PORT=0379&&yarn start
+
++ react: REACT框架的核心部分,提供了component 类可以供我们进行组件开发,提供了钩子函数(生命周期函数: 所有的生命周期函数都是基于回调函数完成的)
++ react-dom: 把JSX语法,渲染为真实的DOM(能够放到页面中展示的结构都叫做真实的DOM)的组件
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-  
+/***
+ * ReactDOM.render([JSX],[CONTAINER], [CALLBACK]);  把JSX元素渲染到页面中
+ *    JSX: REACT 的虚拟元素
+ *    CONTAINER: 容器, 我们想把元素放到页面中的那个容器中
+ *    CALLBACK:  回调
+ */
+
+let root = document.querySelector("#root")
+
+let data = "My React"
+
+ReactDOM.render(<div id="box">Hello { data }</div>, root, () => {
+  let oBox = document.querySelector("#box")
+  console.log(oBox.innerHTML)
+});
