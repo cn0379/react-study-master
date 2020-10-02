4adb69782ad640ead18cb7fa122f154caa1fa26c
cn0379 <cn0379@outlook.com>
2020-10-02 12:16:37 +0800

    code5

--- src/myJSX.js
+++ src/myJSX.js
@@ -1,14 +1,14 @@
-function createElement(type, props, children) {
+function createElement(type, props, ...childrens) {
 
   let obj = {
     type: null,
     props: {
-      children: '',
+      children: childrens.length <= 1 ? (childrens[0] || '') : childrens
     },
     ref: null,
     key: null
   };
-  obj = { ...obj, type, props: { ...props, children } };
+  obj = { ...obj, type, props: { ...props, childrens } };
 
   // let newObj = Object.assign({
   //   type: null,
@@ -22,49 +22,55 @@ function createElement(type, props, children) {
   // 'ref' in obj.props ? (obj.ref = obj.props.ref, obj.props.ref = undefined) : null;
   return obj;
 }
+
 let root = document.querySelector("#root")
 
-let objJSX = createElement('h1', { id: "text", className: "test2", style: { color: 'red' }, ref: 'AA', key: 'BB' }, '\u73E0')
+let objJSX = createElement('h1', { id: "text", className: "test2", style: { color: 'red' }, ref: 'AA', key: 'BB' }, 'My react')
 console.log(objJSX)
 // console.log(createElement({ type: 'h1', props: { id: "text", className: "test2", style: { color: 'red' }, children: '\u73E0', ref: '', key: 'HH' } }))
 function render(obj, container, callBack) {
-     // 结构type是为了获取dom元素名称 props获取属性
-     let { type, props } = obj || {}
-    // 创建一个html 元素
-    let newElement = document.createElement(type)
-     for(let attr in props) {
-      // 如果不是私有属性
-      if(!props.hasOwnProperty(attr)) break;
-      // 如果属性没有值
-      if(!props[attr]) break;
-      // 保存值
-      let value = props[attr];
-      if(attr === 'className') {
+  // 结构type是为了获取dom元素名称 props获取属性
+  let { type, props } = obj || {}
+  // 创建一个html 元素
+  let newElement = document.createElement(type)
+  for (let attr in props) {
+    // 如果不是私有属性
+    if (!props.hasOwnProperty(attr)) break;
+    // 如果属性没有值
+    if (!props[attr]) break;
+    // 保存值
+    let value = props[attr];
+    if (value == undefined) continue;
+
+    switch (attr.toUpperCase()) {
+      case 'CLASSNAME':
         newElement.setAttribute('class', value)
-        continue;
-      }
-      if(attr === 'style') {
-        if(value === '') continue;
-        for(let key in value) {
-          if(value.hasOwnProperty(key)) {
-             newElement['style'][key]  = value[key]
+        break;
+      case 'STYLE':
+        for(let styleAttr in value) {
+          if(value.hasOwnProperty(styleAttr)){
+            newElement['style'][styleAttr] =  value[styleAttr]
           }
         }
+        break;
+        case 'CHILDREN':
+          !(value instanceof Array) ? value = [value] : null;
+    }
+ 
+    if (attr === 'children') {
+      if (typeof value == 'string') {
+        let text = document.createTextNode(value);
+        newElement.appendChild(text);
       }
-      if(attr === 'children') {
-        if(typeof value == 'string') {
-          let text = document.createTextNode(value);
-          newElement.appendChild(text);
-        }
-        continue;
-      }
-      // 让设置的属性表现在DOM结构上
-      newElement.setAttribute(attr, value)
-     }
-     
-     container.appendChild(newElement);
+      continue;
+    }
+    // 让设置的属性表现在DOM结构上
+    newElement.setAttribute(attr, value)
+  }
 
-     callBack && callBack()
+  container.appendChild(newElement);
+
+  callBack && callBack()
 }
 
 render(objJSX, root, () => {
